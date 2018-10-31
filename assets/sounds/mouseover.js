6b0b0d4af152d17f5fe344eb2f0bc3f2fae33dc2
אליהו <helieus02@gmail.com>
2018-10-31 20:52:59 +0200

    Update mouseover.js

--- assets/sounds/mouseover.js
+++ assets/sounds/mouseover.js
@@ -1,2 +1,15 @@
-var audio = document.getElementById("mySoundClip.mp3");
-audio.play();
+function playclip() {
+  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+    if (document.all)
+    {
+     document.all.sound.src = "click.mp3";
+    }
+  }
+
+  else {
+{
+  var audio = document.getElementById("mySoundClip")[0];
+  audio.play();
+    }
+  }
+  }
